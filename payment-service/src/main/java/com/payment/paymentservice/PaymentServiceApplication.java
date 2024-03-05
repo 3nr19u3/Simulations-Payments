8d7dd914ce9669bf276bd8dd7041561106924eb6
3nr19u3 <kike123kob@gmail.com>
2024-03-05 04:53:37 -0500

    add a open feign library to ms comunication

--- payment-service/src/main/java/com/payment/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/payment/paymentservice/PaymentServiceApplication.java
@@ -3,16 +3,18 @@ package com.payment.paymentservice;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PaymentServiceApplication {
 
-	@Bean
-	public RestTemplate restTemplate(){
-		return new RestTemplate();
-	}
+	//@Bean
+	//public RestTemplate restTemplate(){
+	//	return new RestTemplate();
+	//}
 
 	@Bean
 	public ModelMapper modelMapper(){

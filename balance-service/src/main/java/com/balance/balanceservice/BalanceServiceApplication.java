715c0bfa67f7052997a5715817de9e25075b3ff2
3nr19u3 <kike123kob@gmail.com>
2024-04-02 19:34:26 -0500

    this implementation not yet works totally

--- balance-service/src/main/java/com/balance/balanceservice/BalanceServiceApplication.java
+++ balance-service/src/main/java/com/balance/balanceservice/BalanceServiceApplication.java
@@ -3,6 +3,7 @@ package com.balance.balanceservice;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
